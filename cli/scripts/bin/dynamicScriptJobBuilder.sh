8243b8efc04657ba25cc68acf5c1efc8db1dd6c5
Jayesh Patel <jayeshpatel@UTplatformoutlook.onmicrosoft.com>
2024-02-27 17:58:37 +0000

    Updated dynamicScriptJobBuilder.sh

--- cli/scripts/bin/dynamicScriptJobBuilder.sh
+++ cli/scripts/bin/dynamicScriptJobBuilder.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 # Sample code to recursively invoke Boomi scripts based on a configuration file 
 source bin/common.sh
 # Get the file name and the job envirnoment.
@@ -17,40 +18,26 @@ unset env
 
 fileName=$(echo "${file}" | sed -e 's/^.*\///g' -e 's/\.conf.*$//g')
 echoi "Executing configurations for file ${file}."
-notes="${BUILD_PROJECTNAME}_${RELEASE_RELEASENAME}_${BUILD_BUILDNUMBER}: ${notes}"
+
 count=1
  for row in $(cat "${file}" | jq -r '.pipelines[] | @base64');
   do
    
-   json=$(echo ${row} | base64 --decode | jq -r 'to_entries' | \
-     jq --arg count "$count" '. + [{"key": "count", "value": $count}]') 
-   
-     job=$(echo $json | jq -r '.[] |  select(.key | contains("job")).value')
-     env=$(echo $json | jq -r '.[] |  select(.key | contains("env")).value')
+   json=$(echo ${row} | base64 --decode | jq -r 'to_entries' | jq --arg count "$count" '. + [{"key": "count", "value": $count}]') 
+   echoi "Json is $json."
+   job=$(echo $json | jq -r '.[] |  select(.key | contains("job")).value')
+    echoi "Job is $job."
 	 
-	 # Automatically tag azure build info in Boomi deployment notes 
-	 if [[ ! -z "${BUILD_BUILDNUMBER}" ]]
-	 then
-	 	notes=$(echo $json | jq -r '.[] |  select(.key | contains("notes")).value')
+	  # Automatically tag azure build info in Boomi deployment notes 
+	   if [[ ! -z "${BUILD_BUILDNUMBER}" ]]
+	   then
+	    notes=$(echo $json | jq -r '.[] |  select(.key | contains("notes")).value')
      	notes="${BUILD_PROJECTNAME}_${RELEASE_RELEASENAME}_${BUILD_BUILDNUMBER}: ${notes}"
      	json=$(echo ${json} | jq --arg notes "$notes" '. + [{"key": "notes", "value": $notes}]')
-	 fi
-
-     # This check ensure that jobs are triggered for only the allowed env.
-     # This will prevent a development job to trigger builds in production.
-     # If env is null use the JOB ENV as the env
-     if [ -z "${env}"  ]
-     then
-          env="${JOB_ENV}"
-          json=$(echo ${json} | jq --arg env "$env" '. + [{"key": "env", "value": $env}]')
-                    
-     fi
-     if [ "${env}" == "${JOB_ENV}" ]
-     then
- 		   call_script "${job}" "${json}"
-     else
-       echo "Ignoring job ${job} for env ${env} as it does not match the JOB_ENV ${JOB_ENV}."
-     fi
-  
-   count=$(( count + 1 ))
+	   fi
+ 
+     env="${JOB_ENV}"
+     json=$(echo ${json} | jq --arg env "$env" '. + [{"key": "env", "value": $env}]')
+     call_script "${job}" "${json}"
+     count=$(( count + 1 ))
  done
